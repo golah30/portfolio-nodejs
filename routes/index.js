3b1de6c40eac03e4509991ecafa711ad1d02aca9
Dima Kurilenko <golah30@gmail.com>
2018-01-12 13:02:45 +0200

    modified: routes, pug templates (paths)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,42 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('pages/index', { title: 'Express' });
+});
+router.get('/index', function(req, res, next) {
+  res.render('pages/index', { title: 'Express' });
+});
+router.get('/home', function(req, res, next) {
+  res.render('pages/index', { title: 'Express' });
+});
+router.get('/index.html', function(req, res, next) {
+  res.render('pages/index', { title: 'Express' });
+});
+router.get('/home.html', function(req, res, next) {
+  res.render('pages/index', { title: 'Express' });
+});
+//works page
+router.get('/works.html', function(req, res, next) {
+  res.render('pages/works', { title: 'Express' });
+});
+router.get('/works', function(req, res, next) {
+  res.render('pages/works', { title: 'Express' });
+});
+
+//about page
+router.get('/about.html', function(req, res, next) {
+  res.render('pages/about', { title: 'Express' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('pages/about', { title: 'Express' });
+});
+
+//blog page
+router.get('/blog.html', function(req, res, next) {
+  res.render('pages/blog', { title: 'Express' });
+});
+router.get('/blog', function(req, res, next) {
+  res.render('pages/blog', { title: 'Express' });
 });
 
 module.exports = router;
